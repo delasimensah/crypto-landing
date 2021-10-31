6503d51da4abe64683983ddc90c6807b538a5905
delasimensah <mensadelasi@gmail.com>
2021-10-31 17:54:39 +0000

    updates

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const withPlugins = require("next-compose-plugins");
-const optimizedImages = require("next-optimized-images");
-
-const nextConfiguration = {
-  target: "serverless",
-};
-
-module.exports = withPlugins([optimizedImages], nextConfiguration);
